982934a20899b89abc575dc0db5729de166066ad
TuncCu <tuncay-cubukcu@hotmail.de>
2025-12-11 09:56:58 +0100

    linked all css/scripts

---
+++ scripts/db.js
@@ -0,0 +1,27 @@
+let myDishes = [
+    {
+        "name": "Chicken Tikka Massala",
+        "description": "In Joghurt mariniertes, gegrilltes Hähnchenfleisch in aromatischer Masalasauce.",
+        "price": 15.95
+    },
+    {
+        "name": "Pho Bo",
+        "description": "Vietnamesische Rindfleisch-Nudelsuppe mit frischen Kräutern und Reisbandnudeln.",
+        "price": 13.50
+    },
+    {
+        "name": "Pad Thai",
+        "description": "Gebratene Reisnudeln mit Ei, Tamarindensauce, Erdnüssen und Gemüse.",
+        "price": 12.90
+    },
+    {
+        "name": "Shawarma Teller",
+        "description": "Würzig mariniertes Fleisch mit Reis, Salat, Tahina und Knoblauchsauce.",
+        "price": 14.20
+    },
+    {
+        "name": "Kibbeh",
+        "description": "Arabische Bulgur-Fleischbällchen gefüllt mit Hackfleisch, Zwiebeln und Gewürzen.",
+        "price": 11.80
+    }
+];
