b62c275c458b65955452e58c10282c43698e5217
TuncCu <tuncay-cubukcu@hotmail.de>
2025-12-15 10:51:37 +0100

    upload image

--- scripts/templates.js
+++ scripts/templates.js
@@ -1,5 +1,5 @@
 function getMainTemplate(dishesItem){
     return `<h3> ${dishesItem.name} </h3>
             <p>${dishesItem.description} </p>
-            <span>${dishesItem.price.toFixed(2)} € </span> `
+            <span>${dishesItem.price.toFixed(2)} € </span>`
 }
