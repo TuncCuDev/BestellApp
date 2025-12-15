484daadbd237ffe25b9cbface0d437731c296ec0
TuncCu <tuncay-cubukcu@hotmail.de>
2025-12-15 11:03:13 +0100

    fixed style css main dish

--- scripts/templates.js
+++ scripts/templates.js
@@ -1,5 +1,5 @@
 function getMainTemplate(dishesItem){
-    return `<div class="mainDish>
+    return `<div class="mainDish">
             "<h3> ${dishesItem.name} </h3>
             <p>${dishesItem.description} </p>
             <span>${dishesItem.price.toFixed(2)} € </span>
