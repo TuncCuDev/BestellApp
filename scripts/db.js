5c2f34d44bc64e8c3a9973d29303450006ecf0dc
TuncCu <tuncay-cubukcu@hotmail.de>
2025-12-15 10:53:17 +0100

    update img

--- scripts/db.js
+++ scripts/db.js
@@ -3,7 +3,6 @@ let dishes = [
             "name": "Jollof Rice",
             "description": "Westafrikanisches Reisgericht mit Tomaten, Chili und Gewürzen.",
             "price": 12.00
-            "img" :"./img/img/thai-fried-rice-3253027_1280.jpg"
         },
         {
              "name": "Paella",
