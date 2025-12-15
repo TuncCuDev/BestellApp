0f75c6e8e1acbb7d81abdef03f13c7375a6cd385
TuncCu <tuncay-cubukcu@hotmail.de>
2025-12-15 11:38:02 +0100

    refresh main

--- scripts/templates.js
+++ scripts/templates.js
@@ -1,6 +1,6 @@
 function getMainTemplate(dishesItem){
     return `<div class="mainDish">
-            "<h3> ${dishesItem.name} </h3>
+            <h3> ${dishesItem.name} </h3>
             <p>${dishesItem.description} </p>
             <span>${dishesItem.price.toFixed(2)} € </span>
             </div>`
