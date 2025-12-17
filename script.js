674478446991b499ff19f63c942934463665de3b
TuncCu <tuncay-cubukcu@hotmail.de>
2025-12-17 10:30:42 +0100

    added function for button add to basket

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
+
 function renderMenu(){
-    let mainDishesRef = document.getElementById('mainDishes')
+    let mainDishesRef = document.getElementById('mainDishes');
     mainDishesRef.innerHTML = "";
+
     for (let index = 0; index < dishes.length; index++) {
         const dishesItem= dishes[index]
-        mainDishesRef.innerHTML += getMainTemplate(dishesItem)
+        mainDishesRef.innerHTML += getMainTemplate(dishesItem);
     }
 }    
+
+function basketOrder (){
+    let basketDishesRef = document.getElementById('basketOrder')
+    basketDishesRef.innerHTML = "";
+
+    for (let basketIndex = 0; basketIndex < basket.length; basketIndex++) {
+        const basketItem = basket[basketIndex];
+        basketDishesRef += getBasketTemplate(basketItem); 
+    }
+}
+
+function moveToBasket(){
+    document.getElementById('basketOrder').innerHTML += getBasketTemplate(basketItem);
+}
--- scripts/templates.js
+++ scripts/templates.js
@@ -8,3 +8,16 @@ function getMainTemplate(dishesItem){
             <div class="addToBasket" onclick="moveToBasket()">+</div>
             </section>`
 }
+
+function getBasketTemplate(basketItem){
+    return `<section class="basketRow">
+            <section class="basketInfo">
+            <div class="basket-Item-Name"> ${dishesItem.name} </div>
+            <div> class="basket-pls-button">+</div>
+            <div class="basket-Item-Amount"                   </div>
+            <div class="basket-mns-button">-</div>
+            <div class="basket-Item-Price" ${dishesItem.price} </div>
+            <img src="./Favicon/mulleimer.png" class="delete-button"> alt="Delete Button">
+            </section>
+            </section>` 
+}
